3d381844068b0bad5e2199b69fe9dc6ac1921cbb
Bersneva, Svetlana <sbersneva@qulix.com>
2019-10-08 15:44:07 +0300

    lesson 36 step 9 updated

--- lesson36/test_items.py
+++ lesson36/test_items.py
@@ -1,7 +1,7 @@
 import time
+import pytest
 
-
-def test_add_button_exist(browser):
+def test_add_button_exist(browser, language):
     #link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     time.sleep(5)
     button_add_to_basket = browser.find_element_by_class_name("btn-add-to-basket")
