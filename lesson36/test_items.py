ae8790566b7fabe1f2bdf44c6a683c14a1a226e2
Bersneva, Svetlana <sbersneva@qulix.com>
2019-10-08 13:42:40 +0300

    lesson 36 step 9

---
+++ lesson36/test_items.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import pytest
+import time
+
+
+def test_add_button_exist():
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(5)
+    button_add_to_basket = browser.find_element_by_class_name("btn-add-to-basket")
+    assert button_add_to_basket.is_element_present, "'Добавить в корзину' кнопки нет на странице"
