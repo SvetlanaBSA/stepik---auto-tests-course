1a5bb07764faf99eedddec0bc721be49a8774957
Bersneva, Svetlana <sbersneva@qulix.com>
2019-10-08 16:13:31 +0300

    lesson 36 step 9

--- lesson36/confest.py
+++ lesson36/confest.py
@@ -1,5 +1,6 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
@@ -9,28 +10,29 @@ def pytest_addoption(parser):
                      help="Choose browser: chrome or firefox")
 
 
-@pytest.fixture(scope="class")
+@pytest.fixture(scope="function", autouse=True)
 def browser(request):
     browser_name = request.config.getoption("--browser_name")
-    browser = None
+    #browser = None
+    language = request.config.getoption("--language")
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
+    if language == "es" or language == "fr":
+        link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
+    else:
+        raise pytest.UsageError("--language should be fr or es")
+    browser.get(link)
     yield browser
     print("\nquit browser..")
     browser.quit()
 
-
-@pytest.fixture(scope="function")
-def language(request):
-    return request.config.getoption("--language")
-    #if language == "es" or language == "fr":
-    #    link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
-    #else:
-    #    raise pytest.UsageError("--language should be fr or es")
-    #browser.get(link)
--- lesson36/test_items.py
+++ lesson36/test_items.py
@@ -1,17 +1,8 @@
 import time
-import pytest
 
 
-class TestSuite():
-
-    def test_add_button_exist(self, browser, language):
-        language = language
-        if language == "es" or language == "fr":
-            link = f"http://selenium1py.pythonanywhere.com/{language}/catalogue/coders-at-work_207/"
-        else:
-            raise pytest.UsageError("--language should be fr or es")
-        browser.get(link)
-        time.sleep(5)
-        button_add_to_basket = browser.find_element_by_class_name("btn-add-to-basket")
-        assert button_add_to_basket.is_element_present, "Button is NOT exist"
+def test_add_button_exist(browser):
+    time.sleep(5)
+    button_add_to_basket = browser.find_element_by_class_name("btn-add-to-basket")
+    assert button_add_to_basket.is_element_present, "Button is NOT exist"
 
