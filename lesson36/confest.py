ae73198791a2d85a2c909e305fd3dd368954d3d6
Bersneva, Svetlana <sbersneva@qulix.com>
2019-10-08 14:16:50 +0300

    lesson 36 step 9 updated

--- lesson36/confest.py
+++ lesson36/confest.py
@@ -17,14 +17,10 @@ def browser(request):
     user_language = request.config.getoption("--language")
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        options = Options()
-        options.add_experimental_option('prefs', {'intl.language': user_language})
-        browser = webdriver.Chrome(options=options)
+        browser = webdriver.Chrome()
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        fp = webdriver.FirefoxProfile()
-        fp.set_preference("intl.accept_languages", user_language)
-        browser = webdriver.Firefox(firefox_profile=fp)
+        browser = webdriver.Firefox()
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
 
